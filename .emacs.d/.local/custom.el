2595e292be5b2dfd45e48df2886782409c6ff64d
Sergey Nizovtsev <snizovtsev@gmail.com>
2019-05-20 11:34:25 +0300

    Add emacs customize config

---
+++ .emacs.d/.local/custom.el
@@ -0,0 +1,16 @@
+;;; .local/custom.el -*- lexical-binding: t; -*-
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-go-gocode-command "~/go/bin/gocode")
+ '(dired-dwim-target t)
+ '(go-eldoc-gocode "~/go/bin/gocode"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#21242b" :foreground "#bbc2cf" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 128 :width normal :foundry "SRC" :family "Hack")))))
